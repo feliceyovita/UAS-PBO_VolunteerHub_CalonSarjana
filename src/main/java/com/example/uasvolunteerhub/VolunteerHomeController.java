e3c4a984dda369c56e26df14fb6f73304e09b3b6
chyntiaclaudiaa <chyntiaclaudia1809@gmail.com>
2025-06-11 19:22:41 +0700

    login

--- src/main/java/com/example/uasvolunteerhub/VolunteerHomeController.java
+++ src/main/java/com/example/uasvolunteerhub/VolunteerHomeController.java
@@ -1,7 +1,12 @@
 package com.example.uasvolunteerhub;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 import javafx.scene.control.Label;
+
+import java.io.IOException;
 
 public class VolunteerHomeController {
     @FXML
@@ -10,5 +15,19 @@ public class VolunteerHomeController {
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    @FXML
+    protected void onLoginButtonClick() {
+        try {
+            FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/example/uasvolunteerhub/login-view.fxml"));
+            Scene scene = new Scene(fxmlLoader.load(), 400, 300);
+            Stage stage = new Stage();
+            stage.setTitle("Login Page");
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
